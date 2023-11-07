a20067d7e43fa4f55a4b4a93f3a02904cf104786
rhtupr <paulineruth.repe@g.msuiit.edu.ph>
2023-11-07 10:44:34 +0700

    modified add course

--- app/route/coursesroute.py
+++ app/route/coursesroute.py
@@ -1,8 +1,8 @@
-from flask import * 
+from flask import *
 from app.models.courses import *
 from flask_wtf import *
 
-courses_bp = Blueprint ('courses', __name__)
+courses_bp = Blueprint('courses', __name__)
 
 @courses_bp.route('/courses/')
 def courses():
@@ -16,10 +16,15 @@ def add_course():
         coursecode = request.form['coursecode']
         coursename = request.form['coursename']
         collegecode = request.form['collegecode']
-        print(coursecode, coursename, collegecode)
         add_cou(coursecode, coursename, collegecode)
-        return redirect('/courses')
-    return render_template('courses.html')
+        return redirect('/courses')  # Redirect after processing the form data
+    else:
+        # Handle the GET request to display the form
+        colleges = get_college()  # Get the college data here
+        return render_template('addcourse.html', colleges=colleges)  # Pass 'colleges' to the template
+
+    
+
 
 @courses_bp.route('/courses/search', methods=['GET', 'POST'])
 def search_courses():
@@ -33,7 +38,6 @@ def search_courses():
 @courses_bp.route('/courses/delete/<string:coursecode>', methods=['DELETE'])
 def remove_course(coursecode):
     if request.method == 'DELETE':
-        print(coursecode)
         delete_course(coursecode)
         return jsonify({'success': True})
 
@@ -41,12 +45,7 @@ def remove_course(coursecode):
 def edit_course():
     if request.method == 'POST':
         coursecode = request.form.get('course_code')
-        coursename = request.form.get('course_name').title()
+        coursename = request.form.get('course_name')
         collegecode = request.form.get('college_code').upper()
         update_course(coursecode, coursename, collegecode)
         return redirect('/courses/')
-    
-
-
-
-
